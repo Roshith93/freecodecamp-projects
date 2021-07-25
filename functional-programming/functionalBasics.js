b5af31455b0951f0412f6ada3f583fb42b7fa2e9
Roshith R <r.roshith@gmail.com>
2021-07-25 13:13:03 +0530

    Example with filter function

---
+++ functional-programming/functionalBasics.js
@@ -0,0 +1,19 @@
+const persons = [
+  { name: 'Tom', category: 'Animal' },
+  { name: 'Jerry', category: 'Animal' },
+  { name: 'Butterfly', category: 'Insect' },
+  { name: 'Crow', category: 'Bird' },
+  { name: 'Peacock', category: 'Bird' },
+]
+
+let insects = []
+for (let i = 0; i < persons.length; i++) {
+  // console.log(person[i].category)
+  if (persons[i].category === 'Insect') {
+    insects.push(persons[i])
+  }
+}
+console.log(insects)
+
+let animals = persons.filter((el) => el.category === 'Animal')
+console.log(animals)
