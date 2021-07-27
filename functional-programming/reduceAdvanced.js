58a3e904938e33fa511783fef20179aa55a3b1d8
Roshith R <r.roshith@gmail.com>
2021-07-27 20:14:22 +0530

    converting the string into and object

--- functional-programming/reduceAdvanced.js
+++ functional-programming/reduceAdvanced.js
@@ -1,11 +1,34 @@
-var fs = require('fs')
-let userDatas = fs.readFileSync('userData.txt', 'utf8')
-console.log(typeof userDatas)
+// var fs = require('fs')
+// let userDatas = fs.readFileSync('userData.txt', 'utf8')
+// console.log(userDatas.trim().split('\n').map(el => el.split('\t')))
 
-// export const abc = () => "hello"
+// ! splice on every second number and form an array
+export const abc = () => 'hello'
+const str = `mark iron 80 2
+mark blender 100 1
+mark knife 20 4
+Nikita iron 80 1
+Nikita blender 100 1
+Nikita knife 20 4`
+
+// let splitOnNewLine = userDatas.trim().split("\n")
+// let splitOnNewTab = splitOnNewLine.map(el => el.split("\t"))
+
+let valToArr = str.split('\n')
+let mappedValue = valToArr.map((el) => el.split(' '))
+// let splitMore = mappedValue.map(el => el.split('m'))
+
+let createFilterObj = (acc, [name, order, price, quantity, abc], index) => {
+  acc[name] = acc[name] || []
+  acc[name].push({ order, price, quantity })
+  return acc
+}
+
+let finalValue = mappedValue.reduce(createFilterObj, {})
+console.log(finalValue)
 
 // result
-const abc = {
+const tsArr = {
   'mark johans': [
     { name: 'waffle iron', price: '80', quantity: 2 },
     { name: 'waffle iron', price: '80', quantity: 2 },
